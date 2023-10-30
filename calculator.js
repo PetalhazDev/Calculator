dc7c19b1e3a5126f4b7cc398650364c3f05282be
Babatunde Peter <babatundepeter9@gmail.com>
2023-10-30 07:24:13 +0100

    First commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function display(value){
+    document.getElementById("result").value+=value
+}
+
+function clr(){
+    document.getElementById("result").value=""
+    resultDisplayed = false;
+}
+
+/*function evaluate(){
+    var x =document.getElementById("result").value
+    var y = evaluate(x)
+    y = document.getElementById("result").value 
+}*/
+
+
+
+    
